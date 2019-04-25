9223d18bee9c5ca5dcd0c142e2fb28b218ee845b
brian hensley and heather zurek <brian@>
2019-04-25 15:20:18 -0700

    add while loop

--- Scrabble/Models/Program.cs
+++ Scrabble/Models/Program.cs
@@ -7,11 +7,24 @@ namespace Scrabble.Models
   {
     public static void Main()
     {
+      bool scrabbleMaking = true;
+
       Console.WriteLine("Hello welcome to our BOOTY");
-      Console.WriteLine("What is your word?");
-      string userInput = Console.ReadLine().ToUpper();
-      Word newWord = new Word(userInput);
-      Console.WriteLine(newWord.GetValue());
+
+      while (scrabbleMaking)
+      {
+        Console.WriteLine("What is your word?");
+        string userInput = Console.ReadLine().ToUpper();
+        Word newWord = new Word(userInput);
+        Console.WriteLine(newWord.GetValue());
+
+        Console.WriteLine("Do you want to check the value of another word? Press 1, or any letter to exit");
+
+        if (Console.ReadLine() != "1")
+        {
+          scrabbleMaking = false;
+        }
+      }
     }
   }
 }
