7e3a1e1570e0854f250636a8d40793647686ee84
brian hensley and heather zurek <heather@>
2019-04-25 10:32:26 -0700

    2 passing tests

---
+++ Scrabble.Tests/Model.Tests/Scrabble.Tests.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scrabble.Models;
+
+namespace Scrabble.TestTools
+{
+  [TestClass]
+  public class ScrabbleTest// : IDisposible
+  {
+    // public void Dispose()
+    // {
+    //   Gram.ClearAll();
+    // }
+
+    [TestMethod]
+    public void WordConstructor_CreateInstanceOfWord_Word()
+    {
+      Word newWord = new Word("hoser");
+      Assert.AreEqual(typeof(Word), newWord.GetType());
+    }
+
+    [TestMethod]
+    public void AssignCharValue_ReturnNonAlphaInput_False()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      string userInput = "ant";
+      Word testWord = new Word(userInput);
+      Assert.AreEqual(3, testWord.AssignCharValue(userInput));
+    }
+
+  }
+}
---
+++ Scrabble/Models/Scrabble.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Scrabble.Models
+{
+  public class Word
+  {
+    private string _word;
+
+
+    public Word(string word)
+    {
+      _word = word;
+    }
+
+    public int AssignCharValue(string userInput)
+    {
+      return 0;
+    }
+  }
+}
